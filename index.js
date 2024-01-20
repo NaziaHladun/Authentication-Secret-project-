d9f1a748c204517fa210f212b0f31266e3523e09
NaziaHladun <fabulous7351@gmail.com>
2024-01-20 19:30:53 +0200

    Added hashing and salting rounds

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 const db = new pg.Client({
   user: "postgres",
@@ -40,8 +42,14 @@ app.post("/register", async (req, res) => {
     if (checkRes.rows.length > 0){
       res.send("Email already exists. Try logging in.");
     } else {
-      await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, password]);
-      res.render("secrets.ejs")
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log("Error hashing password:", err);
+        } else {
+          await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, hash]);
+          res.render("secrets.ejs");
+        }
+      });
     }
   } catch (err) {
     console.log(err);
@@ -50,19 +58,21 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {  
   let email = req.body.username;
-  let password = req.body.password;
+  let loginPassword = req.body.password;
 
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
-      const storesPassword = user.password;
+      const storesHashPassword = user.password;
 
-      if (storesPassword === password) {
-        res.render("secrets.ejs")
-      } else {
-        res.send("Try another password");
-      }
+      bcrypt.compare(loginPassword, storesHashPassword, (err, result) => {
+        if (result) {
+          res.render("secrets.ejs");
+        } else {
+          res.send("Incorect password");
+        }
+      });
     } else {
       res.send("User not found");
     }
